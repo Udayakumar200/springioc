22d651150b49fef8ff93ef39244edd82741a221c
UDAYAKUMAR R <153817885+Udayakumar200@users.noreply.github.com>
2024-04-29 13:48:17 +0530

    Delete display directory

--- display/src/main/java/com/taskdisplay/display/repo/productRepo.java
+++
@@ -1,7 +0,0 @@
-package com.taskdisplay.display.repo;
-
-import com.taskdisplay.display.entity.Products;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface productRepo  extends JpaRepository<Products ,Long> {
-}
